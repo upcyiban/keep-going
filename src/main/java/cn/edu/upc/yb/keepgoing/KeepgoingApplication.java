4b59f748333375d6bbab970b164838a23dbdee91
skyADMIN <546485299@qq.com>
2016-06-04 16:29:03 +0800

    init project

---
+++ src/main/java/cn/edu/upc/yb/keepgoing/KeepgoingApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.upc.yb.keepgoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepgoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepgoingApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/edu/upc/yb/keepgoing/KeepgoingApplicationTests.java
@@ -0,0 +1,18 @@
+package cn.edu.upc.yb.keepgoing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = KeepgoingApplication.class)
+@WebAppConfiguration
+public class KeepgoingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
