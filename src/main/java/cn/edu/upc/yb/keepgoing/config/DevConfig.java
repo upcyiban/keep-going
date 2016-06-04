6abb0b8181aba32b8af83772e351d2d2015e0276
skyADMIN <546485299@qq.com>
2016-06-04 17:02:30 +0800

    add appid, appsecret and redirect_uri. add util to decode message.

---
+++ src/main/java/cn/edu/upc/yb/keepgoing/config/DevConfig.java
@@ -0,0 +1,10 @@
+package cn.edu.upc.yb.keepgoing.config;
+
+/**
+ * Created by skyADMIN on 16/6/4.
+ */
+public class DevConfig {
+    public static String client_id = "0e4a5357f623de96";
+    public static String AppSecret = "c791cafc8a5762d3dd72c4097332502c";
+    public static String redirect_uri = "http://f.yiban.cn/iapp50585";
+}
